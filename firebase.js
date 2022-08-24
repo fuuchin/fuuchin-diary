6836011687ddc88a6bfdd8cc8b52ca3ac13893e6
Fuuchin <denkisutandokese@outlook.jp>
2022-08-24 19:43:49 +0900

    firebaseアプリの初期設定を追加

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBYbWhvplQwEURSI4k5006RC1hBTCJ3cSs",
+  authDomain: "fuuchin-diary.firebaseapp.com",
+  projectId: "fuuchin-diary",
+  storageBucket: "fuuchin-diary.appspot.com",
+  messagingSenderId: "459932839129",
+  appId: "1:459932839129:web:e24aa1762df88b4c7ba2c8",
+  measurementId: "G-DHZBRYJPKH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+/// ブラウザのみで動作させる
+if (typeof window !== 'undefined') {
+  const analytics = getAnalytics(app);
+}
